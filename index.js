233f4b78c44cf5995b846a7a2ed9c3a570d0bd78
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-30 14:19:36 +0900

    project setup finished

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+const app = express();
+require('./routes/mapInformationRoutes')(app);
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
---
+++ routes/mapInformationRoutes.js
@@ -0,0 +1,6 @@
+module.exports = (app) => { 
+    app.get(
+        '/',
+        () => {console.log("hello world")}
+    ) 
+}
