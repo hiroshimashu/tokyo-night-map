a5f72bd239255dd47355a8848370961b790134f1
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-06 16:13:01 +0900

    fix the problem of non-appearing scatter plots

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,65 +1,92 @@
 import React, { Component } from 'react';
 import axios from  'axios';
-import {StaticMap} from 'react-map-gl'; 
+import MapGL from 'react-map-gl'; 
 import Point from './point';
 // Need to move TOKEN into .env file and read from it. 
+const MAPBOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
 const TOKEN = "pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2o4OW90ZjNuMDV6eTMybzFzbmc3bWpvciJ9.zfRO_nfL1O3d2EuoNtE_NQ";
 class App extends Component {
-    state = {
-        mapinfo: []
+    constructor(props) {
+        super(props);
+        this.state = {
+          viewport: {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            longitude:139.72,
+            latitude: 35.66,
+            zoom: 16,
+            maxZoom: 20,
+            selectedHour: null
+          },
+        };
+        this._resize = this._resize.bind(this);
     }
-
-    componentWillMount() {
-        this.getMapData();
+    
+    componentDidMount() {
+        this._processData();
+        window.addEventListener('resize', this._resize);
+        this._resize();
     }
-
-    getMapData =  async () => {
-        const mapInfo = await axios.get('http://localhost:5000/get/mapinfo');
-        this.setState(() => {
-            return { mapinfo: mapInfo.data }
-        })
-    }
-
-    renderMapData = () => {
-        if (this.state.mapinfo.length === 0) {
-            return <div>no place to be shown yet</div>
+    
+      componentWillUnmount() {
+        window.removeEventListener('resize', this._resize);
+      }
+    
+      _resize() {
+        this._onViewportChange({
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+      }
+    
+      _onSelect(selectedHour) {
+        this.setState({
+          selectedHour: selectedHour === this.state.selectedHour ? null : selectedHour
+        });
+      }
+    
+      async _processData() {
+        const taxiData = await axios.get('http://localhost:5000/get/mapinfo');
+        if (taxiData) {
+          console.log(taxiData);
+          this.setState({status: 'LOADED'});
+          const points = taxiData.data.reduce((accu, cuur) => {
+            accu.push({
+              position: [Number(cuur.longitude), Number(cuur.latitude)],
+              pickup: true
+            })
+            return accu;
+          }, []);
+          this.setState({
+            points,
+            status: 'READY'
+          });
         }
-        const shopList = this.state.mapinfo.map((info) => {
-            return (
-                <div key = {info._id}>
-                    name:{ info.shopName }<br/>
-                    latitude: { info.latitude }<br />
-                    longitude: { info.longitude }<br /> 
-                    category: { info.category } <br/>
-                </div>
-
-            )
-        })
-        return shopList;
-    }
-
-    render() {
-        const data = {coodinates: [35.66082, 139.726211]}
-
-        return(
-            <div>
-               {this.renderMapData()}
-                <StaticMap
-                   width={400}
-                   height={400}
-                   latitude={35.66082}
-                   longitude={139.726211}
-                   zoom={16} 
-                   mapboxApiAccessToken= {TOKEN}
-                >
-                    <Point 
-                        data = {data}
-                        
-                    />
-                </StaticMap>
-            </div>
-        )
-    }
+      }
+    
+      _onViewportChange(viewport) {
+        this.setState({
+          viewport: {...this.state.viewport, ...viewport}
+        });
+      }
+    
+    
+    
+      render() {
+        return (
+          <div>
+          <MapGL
+            {...this.state.viewport}
+            onViewportChange={viewport => this._onViewportChange(viewport)}
+            mapStyle={MAPBOX_STYLE}
+            mapboxApiAccessToken={TOKEN}>
+            <Point
+              data={this.state.points}
+            />
+          </MapGL>
+        </div>
+        );
+      }
 }
 
 export default App;
--- client/src/components/point.js
+++ client/src/components/point.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import DeckGL, { ScatterplotLayer } from 'deck.gl';
+import React, {Component} from 'react';
+import DeckGL, {ScatterplotLayer} from 'deck.gl';
 
-const Point = ({ data, viewport }) => {
-    const layer = new ScatterplotLayer({
-        id: 'scatterplot-layer',
-        data,
-        pickable: true,
-        opacity: 0.8,
-        radiusScale: 6,
-        radiusMinPixels: 1,
-        radiusMaxPixels: 100,
-        getPosition: d => d.coordinates,
-        getRadius: d => 5,
-        getColor: d => [255, 140, 0],
-      });
-    
-      return (<DeckGL {...viewport} layers={[layer]} />);
+const viewport = { 
+    width: window.innerWidth,
+    height: window.innerHeight,
+    longitude:139.72,
+    latitude: 35.66,
+    zoom: 16,
 }
 
-export default  Point;
+export default class DeckGLOverlay extends Component {
+
+  _initialize(gl) {
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+  }
+
+  render() {
+    if (!this.props.data) {
+      return null;
+    }
+
+    const layers = [
+      new ScatterplotLayer({
+        id: 'scatterplot',
+        getPosition: d => d.position,
+        getColor: d => [0, 128, 255],
+        getRadius: d => 5,
+        opacity: 0.5,
+        pickable: false,
+        radiusScale: 5,
+        radiusMinPixels: 0.25,
+        radiusMaxPixels: 30,
+        ...this.props
+      })
+    ];
+
+
+    return (
+      <DeckGL {...viewport} layers={layers} onWebGLInitialized={this._initialize} />
+    );
+  }
+}
