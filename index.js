4684766f15aef414d0a4a0adcde857e705a6d8e0
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-10 18:08:58 +0900

    fix gitignore

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.use(function(req, res, next) {
 
 require('./routes/mapInformationRoutes')(app);
 
+if(process.env.NODE_ENV === "production") {
+  // Express will server up produciton assets 
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will server up the index.html file 
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT || 5000
