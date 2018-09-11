7bd4ea14e775cae51d5230b6f1116c372b358880
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-11 18:36:19 +0900

    add state to DataForm component

--- client/src/components/DataForm.js
+++ client/src/components/DataForm.js
@@ -5,19 +5,30 @@ class DataForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            success: false
+            success: false,
+            shopname:'',
+            latitude: '',
+            longitude: '',
+            category: ''
         }
     }
+
+    handleChange = (e, { name, value}) => this.setState({[name]: value })
+
+    handleSubmit = () => {
+        console.log(this.state);
+    }
+    
     render() {
         return (
-            <Form success = { this.state.success }>
-                <Form.Input label='店の名前' fluid placeholder='alife' />
-                <Form.Input label='緯度' fluid placeholder='39.775' />
-                <Form.Input label='経度' fluid placeholder='１39.333' />
-                <Form.Input label='category' fluid placeholder='Dance club' />
+            <Form success = { this.state.success } onSubmit = { this.handleSubmit }>
+                <Form.Input onChange = { this.handleChange } label='店の名前' name="shopname" fluid placeholder='alife' />
+                <Form.Input onChange = { this.handleChange } label='緯度' name = "latitude" fluid placeholder='39.775' />
+                <Form.Input onChange = { this.handleChange } label='経度' name = "longitude" fluid placeholder='１39.333' />
+                <Form.Input onChange = { this.handleChange } label='category' name = "category" fluid placeholder='Dance club' />
                 <Message success = { this.state.success } header='Form Completed' content="You're all signed up for the newsletter" />
                 <Button>Submit</Button>
-          </Form>
+            </Form>
         )
     }
 }
