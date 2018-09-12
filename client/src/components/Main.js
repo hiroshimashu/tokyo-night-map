58480f320d2de7179e0617e7f2b05730ba95ec74
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-12 16:09:46 +0900

    change filename(App.js -> app.js)

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
-import App from '../components/App';
+import App from '../components/app';
 import DataForm from '../components/DataForm';
 
 
