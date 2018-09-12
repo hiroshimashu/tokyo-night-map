7b0327377e60956a5e6c85e5edb99c3044c9de37
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-12 14:15:50 +0900

    fix model to implement validation

--- models/MapInformation.js
+++ models/MapInformation.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const mapInformationSchema = new Schema({
-    shopName: String,
-    latitude: Number,
-    longitude: Number,
+    shopName: {type: String, required: true },
+    latitude: {type: Number, required: true },
+    longitude: {type: Number, required: true },
     category: String
 });
 
