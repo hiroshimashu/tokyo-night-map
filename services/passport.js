3140d0c410aaef5499446ea730daba3ed3a236b7
hiroshimashu <hiroshimashu@gmail.com>
2018-12-01 17:34:26 +0900

    add serlizer and deserializer

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,24 @@ const InstagramStrategy = require('passport-instagram');
 
 const User = mongoose.model('user');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+})
+
 passport.use(new InstagramStrategy({
     clientID: INSTAGRAM_CLIENT_ID,
     clientSecret: INSTAGRAM_CLIENT_SECRET,
     callbackURL: "http://127.0.0.1:3000/auth/instagram/callback"
   },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ instagramId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ instagramId: profile.id }).then(existingUser => {
+      if(existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ instagramId: profile.id })
+            .save()
+            .then(user => done(null, user));
+      }
+    })
   }
 ));
